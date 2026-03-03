396aa213b8ec56af4a21b51024c89d0a5930f56d
Md. Mahmudul Hasan <mahmu@gmail.com>
2026-03-03 10:42:42 +0600

    database changed to mysql

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,9 @@ DATABASES = {
         'HOST': 'mithun41.mysql.pythonanywhere-services.com', # হোস্ট এড্রেস
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    'charset': 'utf8mb4',
+},
     }
 }
 
