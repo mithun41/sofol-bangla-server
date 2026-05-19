7b59e4b51f40ca87ad7fcacba74365d43a14485b
Md. Mahmudul Hasan <mahmudulhasan3527@gmail.com>
2026-05-19 14:10:40 +0600

    update cloudinary code

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# 🔥 [CRITICAL] পাইথনঅ্যানিহোয়্যারের ফ্রি প্রক্সি এবং ড্যাঙ্গো সেটিংস সবার আগে পুশ করতে হবে
+os.environ["http_proxy"] = "http://proxy.server:3128"
+os.environ["https_proxy"] = "http://proxy.server:3128"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# 🚀 এবার ড্যাঙ্গোর কোর ডব্লিউএসজিআই মেথড ইমপোর্ট এবং রান হবে
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
